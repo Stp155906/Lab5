eb6e4041e794b738aabed0b44cb8240442658214
Shantalia <71754706+Stp155906@users.noreply.github.com>
2023-10-16 22:27:15 -0400

    Your descriptive commit message here

---
+++ ios101-project5-tumblr/TableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TableViewCell.swift
+//  ios101-project5-tumblr
+//
+//  Created by Shantalia Z on 10/16/23.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
